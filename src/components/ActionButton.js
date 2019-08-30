02a4f1377c2052f3344f3041e07dee0855a42e32
Jan Skowronski <crianonim@gmail.com>
2019-08-30 14:08:06 +0100

    Add disabled label to ActionButton

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -3,12 +3,12 @@ import Button from '@material-ui/core/Button'
 import {useDispatch} from 'react-redux';
 import * as conditions from '../game-logic/conditions';
 
-const ActionButton = ({children,action,payload,variant,color})=>{
+const ActionButton = ({children,action,payload,variant,color,disabledLabel})=>{
     const dispatch=useDispatch();
     const canAct=conditions[action]?conditions[action](payload):true;
     return ( <Button variant={variant} color={color} disabled={!canAct} onClick={()=>{
            dispatch({type:action,payload})
-        }}>{children}</Button>
+        }}>{(disabledLabel && !canAct)?disabledLabel : children}</Button>
     )
 }
 
